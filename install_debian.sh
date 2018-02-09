82e74ce683d2d7ce1fbac0d6da76a4d92bd35aad
Frederik Stadler <frederikstadler@gmail.com>
2018-02-09 01:21:48 +0100

    Automatische Sicherheitsaktualisierung

--- install_debian.sh
+++ install_debian.sh
@@ -244,6 +244,9 @@ download_file "script/sockd" "${BIN_SCRIPT}" "execute"
 
 ########################################## DEBIAN 8 ####################################################################
 apt-get update
+apt-get -yt $(lsb_release -cs)-security dist-upgrade
+apt-get -d dist-upgrade 
+apt-get autoclean
 apt-get install unzip apache2-utils gcc g++ make libpam-dev libwrap0-dev -y
 
 mkdir -p /tmp/danted && rm /tmp/danted/* -rf && cd /tmp/danted
